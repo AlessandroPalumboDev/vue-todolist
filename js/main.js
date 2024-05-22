71c90ed2a28e284706a301a56c1603f7895e34f1
AlessandroPalumboDev <alessandropalumbo.dev@gmail.com>
2024-05-22 20:08:13 +0200

    [UPD] Milestone 1

--- js/main.js
+++ js/main.js
@@ -1,1 +1,37 @@
-console.log('sono js')
+const { createApp } = Vue;
+
+createApp({
+    data() {
+        return {
+            list:[
+                {
+                    text: 'Svegliarsi',
+                    done: true
+                },{
+                    text: 'Lavarsi',
+                    done: true
+                },{
+                    text: 'Mangiare',
+                    done: true
+                },{
+                    text: 'Coding',
+                    done: false
+                },{
+                    text: 'Mangiare',
+                    done: false
+                },{
+                    text: 'Dormire',
+                    done: false
+                }
+            ],
+
+            currentIndex: 0,
+        }
+    },
+
+    methods:{
+
+    }
+
+
+    }).mount('#app')
